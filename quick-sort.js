349b2b9f7edce94fbb28c2f2fa8517c95c9a8849
joneirikboska <joneirikboska@gmail.com>
2021-01-01 18:01:08 +0100

    Add quick sort implementation.

---
+++ quick-sort.js
@@ -0,0 +1,28 @@
+const { performance } = require('perf_hooks')
+
+const arr = [9, 2, 22, 90, 4, 1, 5, 7, 8, 11]
+
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr
+  }
+
+  let pivot = arr[0]
+  let lesserArray = []
+  let greaterArray = []
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > pivot) {
+      greaterArray.push(arr[i])
+    } else {
+      lesserArray.push(arr[i])
+    }
+  }
+
+  return quickSort(lesserArray).concat(pivot, quickSort(greaterArray))
+}
+
+const t0 = performance.now()
+console.log(quickSort(arr))
+const t1 = performance.now()
+console.log('Call to quickSort took ' + (t1 - t0).toFixed(2) + ' milliseconds')
